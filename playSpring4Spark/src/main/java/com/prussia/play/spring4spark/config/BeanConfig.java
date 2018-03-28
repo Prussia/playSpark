2cf6db94f27a7ccac2cf16066b0c2a87422feccc
Prussia <prussia.hu@pwc.com>
2018-03-28 15:24:17 +0800

    refactor

--- playSpring4Spark/src/main/java/com/prussia/play/spring4spark/config/BeanConfig.java
+++ playSpring4Spark/src/main/java/com/prussia/play/spring4spark/config/BeanConfig.java
@@ -1,15 +1,14 @@
 package com.prussia.play.spring4spark.config;
 
+import com.prussia.play.spring4spark.config.prop.SparkProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.prussia.play.spring4spark.config.prop.SparkProperties;
 
 @Configuration
 public class BeanConfig {
 
-	@Bean
-	public SparkProperties sparkProperties() {
-		return new SparkProperties();
-	}
+    @Bean
+    public SparkProperties sparkProperties() {
+        return new SparkProperties();
+    }
 }
